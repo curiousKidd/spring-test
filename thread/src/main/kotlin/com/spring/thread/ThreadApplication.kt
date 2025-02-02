fae93e854414544dc5bcf8a024d8d494cbcd38ec
curiousKidd <skasrjhaan@gmail.com>
2025-02-02 23:46:26 +0900

    first commit

---
+++ thread/src/main/kotlin/com/spring/thread/ThreadApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.thread
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThreadApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThreadApplication>(*args)
+}
